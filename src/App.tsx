4efefa55a4e35dd34a2d6ce533a9959539565e33
DESKTOP-HUQ5D35\김인영 <ekseh789@gmail.com>
2024-10-30 14:16:35 +0900

    문법 코드 업로드

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "리액트";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="container">
+    <h1 className='test'>Hello, {name === '리액트' ? (<h1>Yes</h1>) : (<h1>No</h1>)} !!</h1>
+    <p>반갑습니다</p>
+    {/* 주석문 작성 */}
+  </div>ㅎ
   );
+
+  // const port = 1;
+  // return (
+  //   <div>
+  //     {
+  //       port || '포트를 사용하지 않았습니다'
+  //     }
+  //   </div>
+  // )
 }
 
 export default App;
