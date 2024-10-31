dcf04c6916c882154264fe125ff4e3f59a036471
DESKTOP-HUQ5D35\김인영 <ekseh789@gmail.com>
2024-10-31 16:33:35 +0900

    시계 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TodoList from './Todolist';
+import Clock from './Timer';
 function App() {
   let name = "리액트";
-  
-  return (
-  <div className="container">
-    <h1 className='test'>Hello, {name === '리액트' ? (<h1>Yes</h1>) : (<h1>No</h1>)} !!</h1>
-    <p>반갑습니다</p>
-    {/* 주석문 작성 */}
-  </div>ㅎ
-  );
 
-  // const port = 1;
-  // return (
-  //   <div>
-  //     {
-  //       port || '포트를 사용하지 않았습니다'
-  //     }
-  //   </div>
-  // )
+  return (
+    <div className="container">
+      <TodoList></TodoList>
+      <Clock></Clock>
+    </div>
+  );
 }
 
 export default App;
